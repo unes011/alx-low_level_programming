ab6b893135e90cbcb038e4199b4c1346fa9b2a16
unes01 <younesbouazi97@gmail.com>
2023-07-17 17:23:22 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-/**
-*main - entry point
-*return: always 0 (success)
-*/
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+
+int main(void) {
+    printf("\"Programming is like building a multilingual puzzle\n");
+    return 0; // Indicate successful execution by returning 0
 }
+
