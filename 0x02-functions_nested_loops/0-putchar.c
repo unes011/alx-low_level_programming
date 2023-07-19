d89eb5bc86912be9fbf88be2ab182d29c9a8e477
unes01 <younesbouazi97@gmail.com>
2023-07-18 18:23:31 -0700

    c2

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
 *main - netry point 
-* return : always
+*return : always
 */
 int main(void)
 {
@@ -14,6 +14,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
- return (0);
+return (0);
 }
 
