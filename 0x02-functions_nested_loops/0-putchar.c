59f0edd00070ab57d4820c4d6d266c34fa3537e5
unes01 <younesbouazi97@gmail.com>
2023-07-18 17:19:20 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,17 +2,21 @@
 
 int _putchar(char c)
 {
-return putchar(c);
+  return putchar(c);
 }
-int main(void){
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+int main(void)
+{
+  _putchar('_');
+  _putchar('p');
+  _putchar('u');
+  _putchar('t');
+  _putchar('c');
+  _putchar('h');
+  _putchar('a');
+  _putchar('r');
+  _putchar('\n');
+
+  return 0;
 }
+
