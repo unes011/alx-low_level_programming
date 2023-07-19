81b845ceefcb16603bf02330232e0c5b2b42b8c9
unes01 <younesbouazi97@gmail.com>
2023-07-18 19:38:43 -0700

    c2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,23 +7,29 @@
  */
 void print_times_table(int n)
 {
-int i, j, result;
-if (n < 0 || n > 15)
-return;
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-result = i * j;
-if (j != 0)
-printf(", ");
-if (result < 10)
-printf(" ");
-else if (result < 100)
-printf(" ");
-printf("%d", result);
-}
-printf("\n");
-}
+    int i, j, result;
+
+    if (n < 0 || n > 15)
+        return;
+
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            result = i * j;
+
+            if (j != 0)
+                printf(", ");
+
+            if (result < 10)
+                printf(" ");
+            else if (result < 100)
+                printf(" ");
+
+            printf("%d", result);
+        }
+
+        printf("\n");
+    }
 }
 
