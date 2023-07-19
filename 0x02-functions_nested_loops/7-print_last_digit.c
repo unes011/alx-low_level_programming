92cb591376b1170ac6074879a8c86f821bb10b23
unes01 <younesbouazi97@gmail.com>
2023-07-18 19:12:49 -0700

    c2

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,11 +9,9 @@
 int print_last_digit(int n)
 {
 int last_digit;
-if (n < 0)
-last_digit = -n % 10;
-else
 last_digit = n % 10;
+if (last_digit < 0)
+last_digit *= -1;
 _putchar('0' + last_digit);
 return (last_digit);
 }
-
